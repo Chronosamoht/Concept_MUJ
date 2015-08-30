0a9cbdfa1b3119b697368429c7d5a5690e97febd
chronosamoht <chronosamoht@gmail.com>
2015-08-30 02:02:15 +0200

    footer et header

---
+++ application/views/templates/footer.php
@@ -0,0 +1,7 @@
+ <footer>
+ 
+
+<p> Thomas Jalabert </p>
+
+
+ </footer>
---
+++ application/views/templates/header.php
@@ -0,0 +1,32 @@
+
+<head>
+  <meta charset="utf-8">
+  <title>MUJ Concept</title>
+  <link rel="stylesheet" href="../vue/style.css">
+</head>
+
+ 
+ <nav>
+ <div id="titre">
+ 
+  <img src ="../rss.png" alt="rss"/>
+  <h1>Power</h1>
+  
+  </div>
+  
+    <ul>
+    <a href="afficher_flux.php">    <li>
+        Archives
+	</li></a><a href="navimages.php"><li>
+        Moteur de recherche
+    </li></a> <a href="recherche.php"><li> 
+       Participez
+    </li></a><a href="backoffice.php"><li>
+        Backoffice
+    </li></a>
+</ul>   
+</nav>
+
+
+
+
